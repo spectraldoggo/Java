aae82ec76371d5a3d20f980603573bea8b6e7aa5
spectraldoggo <spectral.doggo@protonmail.com>
2026-07-16 00:49:08 -0300

    Update BTree.java

--- src/main/java/com/thealgorithms/datastructures/trees/BTree.java
+++ src/main/java/com/thealgorithms/datastructures/trees/BTree.java
@@ -5,11 +5,9 @@ import java.util.ArrayList;
 /**
  * Implementation of a B-Tree, a self-balancing tree data structure that maintains sorted data
  * and allows searches, sequential access, insertions, and deletions in logarithmic time.
- *
- * B-Trees are generalizations of binary search trees in that a node can have more than two children.
+  * B-Trees are generalizations of binary search trees in that a node can have more than two children.
  * They're widely used in databases and file systems.
- *
- * For more information: https://en.wikipedia.org/wiki/B-tree
+  * For more information: https://en.wikipedia.org/wiki/B-tree
  */
 
 public class BTree {
